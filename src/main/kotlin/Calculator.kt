dd369f46d9f6f0014f0f03c969116069536136ad
Tony Castellano <tcshao@gmail.com>
2017-10-27 23:52:08 -0700

    fixed linting errors

--- src/main/kotlin/Calculator.kt
+++ src/main/kotlin/Calculator.kt
@@ -1,24 +1,23 @@
 package com.example
 
 interface Result {
-  fun write(answer: Int)
+    fun write(answer: Int)
 }
 
 class Calculator(val output: Result) {
-  var total = 0
+    var total = 0
 
-  fun add(x: Int, y: Int): Int {
-    return x + y
-  }
+    fun add(x: Int, y: Int): Int {
+        return x + y
+    }
 
-  fun accumulate(x: Int) {
-    total += x
-    output.write(total)
-  }
+    fun accumulate(x: Int) {
+        total += x
+        output.write(total)
+    }
 }
 
 class NullResult : Result {
-  override fun write(answer: Int) {
-  }
+    override fun write(answer: Int) { }
 }
 
--- src/test/kotlin/CalculatorSpek.kt
+++ src/test/kotlin/CalculatorSpek.kt
@@ -3,42 +3,42 @@ package com.example
 import com.nhaarman.mockito_kotlin.mock
 import com.nhaarman.mockito_kotlin.verify
 import org.jetbrains.spek.api.Spek
-import org.jetbrains.spek.api.dsl.*
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.it
 import org.amshove.kluent.shouldEqual
 
 class CalculatorSpek : Spek({
-  describe("the calculator") {
+    describe("the calculator") {
+        var calculator: Calculator? = null
 
-    var calculator: Calculator? = null
+        beforeEachTest {
+            calculator = Calculator(NullResult())
+        }
 
-    beforeEachTest {
-      calculator = Calculator(NullResult())
+        it("should add two numbers") {
+            var result = calculator?.add(12, 13)
+            result shouldEqual 25
+        }
+
+        it("should accumulate one number") {
+            calculator?.accumulate(23)
+            calculator?.total shouldEqual 23
+        }
+
+        it("should accumulate two numbers") {
+            calculator?.accumulate(2)
+            calculator?.accumulate(4)
+            calculator?.total shouldEqual 6
+        }
     }
 
-    it("should add two numbers") {
-      var result = calculator?.add(12, 13)
-      result shouldEqual 25
-    }
+    describe("the output should be written correctly") {
+        var result: Result = mock()
+        val calculator = Calculator(result)
 
-    it("should accumulate one number") {
-      calculator?.accumulate(23)
-      calculator?.total shouldEqual 23
+        it("should write the correct amount") {
+            calculator.accumulate(23)
+            verify(result).write(23)
+        }
     }
-
-    it("should accumulate two numbers") {
-      calculator?.accumulate(2)
-      calculator?.accumulate(4)
-      calculator?.total shouldEqual 6
-    }
-  }
-
-  describe("the output should be written correctly") {
-    var result: Result = mock()
-    val calculator = Calculator(result)
-
-    it("should write the correct amount") {
-      calculator.accumulate(23)
-      verify(result).write(23)
-    }
-  }
 })
